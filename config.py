3b3ae8bb6081593d9d6bfcc091f31024b114053d
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-02-26 23:10:29 +0800

    Remove environment variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'ZN\x86\xb9\xa5\x8c\x83\t *\x8eD\x89\xc1\x8a\t\x90\xd4\r\xb2\xb3\xe6\x8bU'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
