74c15fec8636aeb7bb91d94d031b59cd63ad2509
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-02-26 15:25:38 +0800

    Add user authentication

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 # import Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 # create the application object
 app = Flask(__name__)
 
+# config
+app.secret_key = "it's a secret"
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 # use decorators to link the function to a url
 @app.route('/')
+@login_required
 def home():
-    return "Hello, world!"
+    # return "Hello, world!"
+    return render_template("index.html")
 
 @app.route('/welcome')
 def welcome():
@@ -20,8 +37,17 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid credentials. Please try again.'
         else:
+            session['logged_in'] = True
+            flash('You were just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
